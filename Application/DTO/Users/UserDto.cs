99735e2e571174e5d1b280e8964770b5381b59f2
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-12-28 17:29:23 +0200

    Add AccountEnabled property to UserDto and update version
    - Added `AccountEnabled` property to `UserDto` class in `UserDto.cs`.
    - Incremented version number in `IdentityX.csproj` from `1.2.6` to `1.2.7`.
    - Updated package release notes in `IdentityX.csproj` to indicate a fix for updating the user object.

--- Application/DTO/Users/UserDto.cs
+++ Application/DTO/Users/UserDto.cs
@@ -5,6 +5,7 @@
 		public string Id { get; set; }
 		public string Email { get; set; }
 		public string Username { get; set; }
+		public bool AccountEnabled { get; set; }
 		public string Role { get; set; }
 		public List<string> Permissions { get; set; }
 		public string Token { get; set; }
