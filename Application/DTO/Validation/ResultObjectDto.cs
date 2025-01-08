cba50dfccbded99aea4fa259852f50774d9be6a5
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2025-01-08 10:29:20 +0200

    Update ResultObjectDto and bump version to 1.3.0
    - Modify ResultObjectDto.cs to use Newtonsoft.Json for Error property serialization.
    - Change Error property to private field _error with public JSON-serialized property.
    - Update IdentityX.csproj version from 1.2.9 to 1.3.0.

--- Application/DTO/Validation/ResultObjectDto.cs
+++ Application/DTO/Validation/ResultObjectDto.cs
@@ -1,9 +1,16 @@
-﻿namespace IdentityX.Application.DTO.Validation
+﻿using Newtonsoft.Json;
+
+namespace IdentityX.Application.DTO.Validation
 {
 	public class ResultObjectDto<T>
 	{
 		public T Result { get; set; }
 
-		public string Error { get; set; }
+		private string _error;
+		public string Error 
+		{ 
+			get { return JsonConvert.SerializeObject(_error); } 
+			set { _error = value; } 
+		}
 	}
 }
